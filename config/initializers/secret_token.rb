7fc87b5c9234929362be3fff72808e75006f8b01
Rob Cameron <cannikinn@gmail.com>
2011-05-05 21:18:16 -0700

    Build rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchmemakethis::Application.config.secret_token = '8e092e0bc7d948bfff439ebc4d637dd7695f0822b788b555d11c832b2f37dc0308fb669c827a5d2106de8d3a6d949ff6344823090fdc9d7449a6d6381a3c8fcc'
