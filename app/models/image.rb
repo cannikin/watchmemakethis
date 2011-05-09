6ac61f5dd9e56a0c3ea18bbf6a25a1dfabfb1c3e
Rob Cameron <cannikinn@gmail.com>
2011-05-09 15:31:51 -0400

    Automatically uploading files when image.save

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,8 @@
 class Image < ActiveRecord::Base
   
   belongs_to :build
-  
-  validates :filename, :presence => true
-  
-  # before_save :put
+
+  before_save :put
   
   # for holding a file that's about to be uploaded (this isn't actually saved to the DB)
   attr_accessor :file
@@ -23,7 +21,7 @@ class Image < ActiveRecord::Base
   
   # the full URL
   def url(size=:small)
-    S3_CONFIG[:cdn] + self.full_path_prefix + '/' + META[size][:prefix] + self.filename
+    S3_CONFIG[:cdn] + '/' + self.full_path_prefix + '/' + META[size][:prefix] + self.filename
   end
   
   
@@ -31,10 +29,10 @@ class Image < ActiveRecord::Base
     if self.file
       remote_filename = UUID.new.generate + '.' + OUTPUT_FORMAT # filename becomes the timestamp to avoid name conflicts
       META.each do |size,options|
-        @tempfile = Tempfile.new(['watch','.jpg'])
-        @tempfile << File.read(self.file)
-        resize(options) if options[:width]
-        upload(options[:prefix]+remote_filename)
+        tempfile = Tempfile.new(['watch','.jpg'])
+        tempfile << File.read(self.file)
+        resize(tempfile, options) if options[:width]
+        upload(tempfile, options[:prefix]+remote_filename)
       end
       self.filename = remote_filename
     else
@@ -45,11 +43,11 @@ class Image < ActiveRecord::Base
   
   # Creates and uploads a single image (the model should know everything it needs to about an image, including how to save itself wherever)
   # Returns the name of the file it just uploaded (or throws an error if something goes wrong)
-  def resize(options)
+  def resize(tempfile, options)
     puts "Resizing #{options.inspect}"
     resize_command = "#{options[:width]}x#{options[:height]}>"
     
-    command = %Q{mogrify -resize "#{resize_command}" #{@tempfile.path}}
+    command = %Q{mogrify -resize "#{resize_command}" #{tempfile.path}}
     sub = Subexec.run(command, :timeout => 5)
     
     puts "exitstatus: #{sub.exitstatus}, output: #{sub.output}"
@@ -58,10 +56,10 @@ class Image < ActiveRecord::Base
   
   
   # takes the current instance and upload its attached file to S3
-  def upload(filename)
+  def upload(tempfile, filename)
     puts "Uploading filename..."
     s3_path = File.join(self.full_path_prefix, filename)
-    AWS::S3::S3Object.store(s3_path, @tempfile.open, S3_CONFIG[:bucket_name], :access => :public_read)   # reopen the tempfile and write directly to S3
+    AWS::S3::S3Object.store(s3_path, tempfile.open, S3_CONFIG[:bucket_name], :access => :public_read)   # reopen the tempfile and write directly to S3
     puts "** Uploaded image to S3: #{s3_path}"
   end
   private :upload
