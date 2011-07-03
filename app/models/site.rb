79d1ac35871ab85a754089f532a0cb8606b9f0ee
Rob Cameron <cannikinn@gmail.com>
2011-07-03 16:06:50 -0700

    validates_format_of for site path

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,7 +6,10 @@ class Site < ActiveRecord::Base
   belongs_to  :style
   
   validates :name, :presence => true, :length => { :maximum => 32, :minimum => 1 }
-  validates :path, :presence => true, :length => { :maximum => 32, :minimum => 1 }, :exclusion => { :in => %w{admin api blog build forgot help images info login logout pricing reset signup site style styles theme themes}, :message => 'This URL is reserved, please try another.' }
+  validates_presence_of   :path
   validates_uniqueness_of :path, :message => 'This URL is already taken, please try another.'
+  validates_length_of     :path, :in => 1..32
+  validates_exclusion_of  :path, :in => %w{admin api blog build forgot help images info login logout pricing reset signup site style styles theme themes}, :message => 'This URL is reserved, please try another.' 
+  validates_format_of     :path, :with => /^[\w-]+$/, :message => 'Your site name may only include letters, numbers, _underscores_ and -dashes-'
   
 end
